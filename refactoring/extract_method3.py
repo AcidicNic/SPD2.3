9dc00c7b5f2c226f4b685c7c3890df8d09b356c2
Nicole Abadie <nic4096@gmail.com>
2021-02-04 17:23:22 -0800

    extract_method3.py refactoring finished

--- refactoring/extract_method3.py
+++ refactoring/extract_method3.py
@@ -1,21 +1,16 @@
 # Written by Kamran Bigdely
 # Example for Compose Methods: Extract Method.
 import math
-xc1 = 4
-yc1 = 4.25
 
-xc2 = 53
-yc2 = -5.35
-# Calculate the distance between the two circle
-distance = math.sqrt((xc1-xc2)**2 + (yc1 - yc2)**2)
-print('distance', distance)
-# *** somewhere else in your program ***
-xa = -36
-ya = 97
+def get_circle_distance(xc1, yc1, xc2, yc2):
+    """Returns the distance between the two circles."""
+    return math.sqrt((xc1-xc2)**2 + (yc1 - yc2)**2)
 
-xb = .34
-yb = .91
-# calcualte the length of vector AB vector which is a vector between A and B points.
-length = math.sqrt((xa-xb)*(xa-xb) + (ya-yb)*(ya-yb))
-print('length', length)
- 
+print(f'distance: {get_circle_distance(4, 4.25, 53, -5.35)}')
+
+def get_vector_length(xa, ya, xb, yb):
+    """Returns the length of vector AB vector
+    which is a vector between A and B points."""
+    return math.sqrt((xa-xb)*(xa-xb) + (ya-yb)*(ya-yb))
+
+print(f'length: {get_vector_length(-36, 97, .34, .91)}')
