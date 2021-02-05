de424e9678a00010eb541f4d5d6a506228a808b3
Nicole Abadie <nic4096@gmail.com>
2021-02-04 17:22:59 -0800

    replace_temp_with_query_fowler.py refactoring finished

--- refactoring/replace_temp_with_query_fowler.py
+++ refactoring/replace_temp_with_query_fowler.py
@@ -1,13 +1,14 @@
 # Adapted from a Java code in the "Refactoring" book by Martin Fowler.
 # Replace temp with query
 # Code snippet. Not runnable.
-def get_price():
-    base_price = quantity * item_price
-    discount_factor = 0
-    if base_price > 1000:
-        discount_factor = 0.95
-    else:
-        discount_factor = 0.98
-    return base_price * discount_factor
 
- 
+def get_discount_factor(base_price):
+    """Returns the discount factor for 'base_price'."""
+    if base_price > 1000:
+        return 0.95
+    return 0.98
+
+def get_price():
+    """Returns the total price of discounted items."""
+    base_price = quantity * item_price
+    return base_price * get_discount_factor(base_price)
