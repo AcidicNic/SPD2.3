56d39a8a7998f4b11368cb84b3a5a6a9d37f6812
Nicole Abadie <nic4096@gmail.com>
2021-02-17 21:45:39 -0800

    finished refactoring /refactoring-other-techniques

--- refactoring-other-techniques/extract_superclass.py
+++ refactoring-other-techniques/extract_superclass.py
@@ -1,49 +1,43 @@
 # By Kamran Bigdely
 # Extract superclass
-class AngryMushroom:
-    
+
+class Enemy:
+
     def __init__(self):
         self.health = 100
-        
+
     def take_damage(self, damage):
         self.health -= damage
-        
+
+class AngryMushroom(Enemy):
+
     def spread_poison(self):
         print('spreading poison!')
 
 
-class AngryBot:
-    
+class AngryBot(Enemy):
+
     def __init__(self):
-        self.health = 100
+        super().__init__()
         self.n_bullets = 40
-        
-    def take_damage(self, damage):
-        self.health -= damage
-        
+
     def punch_iron_fist(self):
         print("punching with iron fist!")
-        
+
     def shoot(self):
         print("shot a bullet!")
         self.n_bullets -= 1
 
 
 
-class AgressiveAlligator:
-    
-    def __init__(self):
-        self.health = 100
-        
-    def take_damage(self, damage):
-        self.health -= damage
-        
+class AgressiveAlligator(Enemy):
+
     def bite(self):
         print('bitting!')
 
 
-angryMushroom = AngryMushroom()
-print("initial health level:", angryMushroom.health)
-angryMushroom.take_damage(25)
+AngryBot = AngryBot()
+print("initial health level:", AngryBot.health)
+AngryBot.take_damage(25)
 print("took damage!")
-print("current health level:",angryMushroom.health)
+print("current health level:",AngryBot.health)
--- refactoring-other-techniques/replace_magic_numbers_with_named_constants.py
+++ refactoring-other-techniques/replace_magic_numbers_with_named_constants.py
@@ -1,14 +1,20 @@
-# by Kami Bigdely 
+# by Kami Bigdely
 # Replace magic numbers with named constanst
+
+# i dont know what this is
+SOME_PHYSICS_CONSTANT = 8.9875517923*1e9
 
 # First Section
 # Given two point charges, calcualte the electric force exerted on them.
 q1 = int(input('Enter a value of charge q1: '))
 q2 = int(input('Enter a value of charge q2: '))
-distance = int(input("Enter the distance be10tween two charges: "))
-force = 8.9875517923*1e9 * q1 * q2/(distance**2)
+distance = int(input("Enter the distance between two charges: "))
+
+force = SOME_PHYSICS_CONSTANT * q1 * q2/(distance**2)
 print ("Electric Force between q1 and q2 is: ", force, "Newton")
+
 # Second Section
+# what magic number???????????????
 num = int(input('Enter an integer number: '))
 if num % 2 == 0:
     print(num, "is an even number.")
